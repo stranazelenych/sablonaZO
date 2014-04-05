54b116f59deb77d5d54879bbb83eb244f35aa801
Libor Vaněk <lv@liborvanek.cz>
2014-04-05 12:57:53 +0200

    index.php - struktura

--- wp-content/themes/SablonaZO-KO/index.php
+++ wp-content/themes/SablonaZO-KO/index.php
@@ -1,4 +1,36 @@
 <?php get_header(); ?>
 
+<div id="menu">
+	<ul role="menu">
+		<li><a href="#">kalendář akcí</a></li>
+		<li><a href="#">vedení ko zlín</a></li>
+		<li><a href="#">základní organizace</a></li>
+		<li><a href="#">naši zastupitelé</a></li>
+		<li><a href="#">dokumenty</a></li>
+		<li><a href="#">zpravodaj</a></li>
+	</ul>
+</div>
+
+<!-- box vlevo nahoře -->
+<div id="promo-box">
+	<h2>Dočasný box pro důležité téma (volby)</h2>
+	<a href="#">Seznamte se: naši kandidáti</a>
+	<a href="#">Kalendář předvolebních akcí</a>
+	<a href="#">Zahájení kampaně: fotogalerie</a>
+</div>
+
+<!-- box vpravo nahoře -->
+<div id="media-box">
+</div>
+
+<!-- box vlevo articles -->
+<div id="articles">
+</div>
+
+<!-- sloupce vpravo feeds -->
+<div id="feeds">
+</div>
+
+
 <?php get_footer(); ?>
 
