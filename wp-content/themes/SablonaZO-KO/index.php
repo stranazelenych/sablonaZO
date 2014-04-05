f7ed7442e90ac6c3683baafa5cfdf95a6260bad3
Jan Sládek <me@honzasladek.com>
2014-04-05 12:36:50 +0200

    Upravy layout

--- wp-content/themes/SablonaZO-KO/index.php
+++ wp-content/themes/SablonaZO-KO/index.php
@@ -1,17 +1,4 @@
 <?php get_header(); ?>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>  
-
-<div id="topstory">
-   <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>           
-   <div id="topstory-text"><?php the_content(); ?></div>
-</div>
-
-<div id="content">
-
-</div>
-
-<?php endwhile; ?>
-<?php endif; ?>  
 
 <?php get_footer(); ?>
 
