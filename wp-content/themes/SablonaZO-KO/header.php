475fbaa255c56fd38dd19474a3df7247ebbd9fa1
adman01 <adman@centrum.cz>
2014-04-06 16:05:26 +0200

    oprava

--- wp-content/themes/SablonaZO-KO/header.php
+++ wp-content/themes/SablonaZO-KO/header.php
@@ -6,7 +6,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title><?php echo get_bloginfo( 'name' ); ?><?php wp_title( '|', true, 'right' ); ?></title>
+        <title><?php wp_title( ' | ', true, 'right' ); ?><?php echo get_bloginfo( 'name' ); ?></title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width,initial-scale=1">
 
