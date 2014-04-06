aa9a1933bfa1be2630e5ec3e7b7ab83ac2fa37b5
adman01 <adman@centrum.cz>
2014-04-06 16:15:54 +0200

    menu na single

--- wp-content/themes/SablonaZO-KO/single.php
+++ wp-content/themes/SablonaZO-KO/single.php
@@ -8,6 +8,17 @@ Template Name: homepage
 
 <div class="container main">
 
+<div id="menu">
+	<div class="menu-button">Menu</div>
+	<nav>
+		<?php wp_nav_menu( array(
+	        'theme_location' => 'primary',
+	        'menu_class' => 'flexnav', //Adding the class for FlexNav
+	        'items_wrap' => '<ul data-breakpoint="800" id="%1$s" class="%2$s">%3$s</ul>'));
+		?>
+  </nav>
+</div>
+
 <div class="content-wrapper">
 
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
