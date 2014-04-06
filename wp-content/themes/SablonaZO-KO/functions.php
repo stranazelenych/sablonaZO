7725ed062a4a169405dd2c223b87fec046a0ae06
Jan Sládek <me@honzasladek.com>
2014-04-06 18:55:31 +0200

    homepage box

--- wp-content/themes/SablonaZO-KO/functions.php
+++ wp-content/themes/SablonaZO-KO/functions.php
@@ -54,9 +54,9 @@ function add_widget_areas(){
     register_sidebar( array(
         'name' => __('Hlavní stránka - nahoře vlevo'),
         'id' => 'home-top-left',
-        'before_widget' => '<div class="hometop left box sedy">',
+        'before_widget' => '<div class="hometop box sedy">',
         'after_widget' => '</div>',
-        
+
     ));
     register_sidebar( array(
         'name' => __('Hlavní stránka - nahoře vpravo'),
