05edbfa7c3001aefc630c063fb4dc118779ae964
Michal Kobelka <michalkobelka@gmail.com>
2014-04-29 07:39:40 +0200

    Article thumbnails

--- wp-content/themes/SablonaZO-KO/loop-home.php
+++ wp-content/themes/SablonaZO-KO/loop-home.php
@@ -2,11 +2,21 @@
 <!-- Query sticky post. -->
 <?php query_posts( array('showposts'=>'7') ); ?>
 
-<?php $i=1; ?>
+<?php $i = 1; ?>
 <?php if ( have_posts() ): while ( have_posts() ) : the_post(); ?>   
-<?php if ($i++==1)$highlight="highlight";else $highlight=""; ?>           
-    	<article class="<?php echo $highlight ?>">
-		     <a href="<?php the_permalink() ?>"><?php the_post_thumbnail('full'); ?></a>
+<?php $featured = ($i++ == 1) ? true : false; ?>  
+
+        <article <?php if ($featured) {echo "class='highlight'";} ?>>
+            <a href="<?php the_permalink() ?>">
+                <?php
+                if ($featured) {
+                    the_post_thumbnail('article-featured');
+                } else {
+                    the_post_thumbnail();
+                }
+                ?>
+                         
+                     </a>
 		     <h2><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h2>
 		     <p class="perex"><?php the_excerpt(); ?></p>
 	    </article> 
