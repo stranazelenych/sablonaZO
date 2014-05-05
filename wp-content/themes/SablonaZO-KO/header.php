923d4bb3a3aabf661824c213006104b460635042
Michal Kobelka <michalkobelka@gmail.com>
2014-05-05 19:57:41 +0200

    Header links

--- wp-content/themes/SablonaZO-KO/header.php
+++ wp-content/themes/SablonaZO-KO/header.php
@@ -40,29 +40,41 @@
 			<div class="yellow"></div>
 			<div class="orange"></div>
 		</div>
-    <?php $imageurl = get_field('headerbg','option'); if($imageurl == ""){$noimg = true;}else{$noimg = false;}?>
-		<div id="header-wrap" <?php if(!$noimg){echo 'style="background: url(' . get_field('headerbg','option') . ')center no-repeat">';}
-      else { echo '>';}?>
-			<div id="header">
-        <?php if(get_field('headerlogo','option')):  ?>
-				<div class="logo">
-					<a href="<?php bloginfo( 'url' ); ?>" class="ctyrlistek"></a>
-					<hgroup>
-                        <a href="<?php bloginfo( 'url' ); ?>">
-                            <h1>Strana zelených</h1>
-                            <h2><?php echo get_field('header2','option');  ?></h2>
-                        </a>
-                    </hgroup>
-					<div class="social">
-					</div>
-				</div>
-        <?php endif; ?>
-        <?php if(get_field('headerright','option')): ?>
-				<div class="icons">
-					<a href="#" class="clenove">Staňte se členy</a>
-					<a href="#" class="podporte">Podpořte nás</a>
-					<a href="#" class="info">Informujte se</a>
-				</div>
-        <?php endif; ?>
-			</div>
-		</div>
+        <?php $imageurl = get_field('headerbg', 'option');
+        if ($imageurl == "") {
+            $noimg = true;
+        } else {
+            $noimg = false;
+        } ?>
+        
+        <div id="header-wrap" <?php if (!$noimg) {
+            echo 'style="background: url(' . get_field('headerbg', 'option') . ') center no-repeat">';
+        } else {
+            echo '>';
+        }
+        ?>
+             
+             <div id="header">
+            <?php if (get_field('headerlogo', 'option')): ?>
+                    <div class="logo">
+                        <a href="<?php bloginfo('url'); ?>" class="ctyrlistek"></a>
+                        <hgroup>
+                            <a href="<?php bloginfo('url'); ?>">
+                                <h1>Strana zelených</h1>
+                                <h2><?php echo get_field('header2', 'option'); ?></h2>
+                            </a>
+                        </hgroup>
+                        <div class="social">
+                        </div>
+                    </div>
+            <?php endif; ?>
+                 
+            <?php if(get_field('headerright','option')): ?>
+                    <div class="icons">
+                            <a href="<?php echo get_field('header-link-members', 'option'); ?>" class="clenove">Staňte se členy</a>
+                            <a href="<?php echo get_field('header-link-support', 'option'); ?>" class="podporte">Podpořte nás</a>
+                            <a href="<?php echo get_field('header-link-inform', 'option'); ?>" class="info">Informujte se</a>
+                    </div>
+            <?php endif; ?>
+	</div>
+    </div>
