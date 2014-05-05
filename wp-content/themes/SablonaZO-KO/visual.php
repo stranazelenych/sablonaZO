92596329b2e51b8f47987e59392c6a7409b79772
Michal Kobelka <michalkobelka@gmail.com>
2014-05-05 21:22:47 +0200

    Another bug fix

--- wp-content/themes/SablonaZO-KO/visual.php
+++ wp-content/themes/SablonaZO-KO/visual.php
@@ -24,7 +24,7 @@
     
         <style>
             .visual {
-                background-image: url(<?php get_field('footer-image', 'option') ?>);
+                background-image: url(<?php echo get_field('footer-image', 'option') ?>);
             }
         </style>
     
