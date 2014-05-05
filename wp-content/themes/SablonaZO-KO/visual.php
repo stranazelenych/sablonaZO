24c9f5571a59935c746bea6fa925a18166db5cde
Michal Kobelka <michalkobelka@gmail.com>
2014-05-05 11:51:04 +0200

    Splash

--- wp-content/themes/SablonaZO-KO/visual.php
+++ wp-content/themes/SablonaZO-KO/visual.php
@@ -9,8 +9,14 @@
 <div class="visual">
     
     
+    
     <?php
-
+    // Splash things
+    if ( is_page_template('page-template-splash.php') ) { ?>
+        <a href="<?php echo get_permalink( 669 ) ?>" class="splash-continue">Pokračovat na web »</a>
+    <?php } ?>
+    
+    <?php
     // Splash things
     if ( is_page_template('page-template-splash.php') ) { ?>
 
@@ -45,13 +51,6 @@
         <?php endif; ?>
     </div>
     
-    
-    <?php
-
-    // Splash things
-    if ( is_page_template('page-template-splash.php') ) { ?>
-        <a href="<?php echo get_permalink( 669 ) ?>" class="splash-continue">Pokračovat na web »</a>
-    <?php } ?>
     
     
 </div>
