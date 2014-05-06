c880d19e552256eaad140df9bd71a4ff2354ef93
Michal Kobelka <michalkobelka@gmail.com>
2014-05-06 16:58:18 +0200

    Pagination

--- wp-content/themes/SablonaZO-KO/functions.php
+++ wp-content/themes/SablonaZO-KO/functions.php
@@ -163,7 +163,7 @@ function paginate($pages = '', $range = 2)
      {
          echo "<div class='pagination'>";
 //         if($paged > 2 && $paged > $range+1 && $showitems < $pages) echo "<a href='".get_pagenum_link(1)."'>&laquo;</a>";
-         if($paged > 1 /*&& $showitems < $pages*/) echo "<a href='".get_pagenum_link($paged - 1)."'>&lsaquo;</a>";
+         if($paged > 1 /*&& $showitems < $pages*/) echo "<a href='".get_pagenum_link($paged - 1)."'>&laquo; Předchozí stránka</a>";
 
          for ($i=1; $i <= $pages; $i++)
          {
@@ -173,7 +173,7 @@ function paginate($pages = '', $range = 2)
              }
          }
 
-         if ($paged < $pages /*&& $showitems < $pages*/) echo "<a href='".get_pagenum_link($paged + 1)."'>&rsaquo;</a>";
+         if ($paged < $pages /*&& $showitems < $pages*/) echo "<a href='".get_pagenum_link($paged + 1)."'>Další stránka &raquo;</a>";
 //         if ($paged < $pages-1 &&  $paged+$range-1 < $pages && $showitems < $pages) echo "<a href='".get_pagenum_link($pages)."'>&raquo;</a>";
          echo "</div>\n";
      }
