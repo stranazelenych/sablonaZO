c48abd03de86fe19f604798fa8549a649ee60fdd
Michal Kobelka <michalkobelka@gmail.com>
2014-05-06 16:24:12 +0200

    Tag archive

--- wp-content/themes/SablonaZO-KO/tag.php
+++ wp-content/themes/SablonaZO-KO/tag.php
@@ -27,7 +27,7 @@ Template Name: Homepage
     
         <?php if ( have_posts() ) : ?>
                 <header class="archive-header">
-                        <h1 class="archive-title"><?php printf( __( 'Tag Archives: %s', 'twentytwelve' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
+                        <h1 class="archive-title"><?php printf( __( 'Kategorie: %s' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
 
                 <?php if ( tag_description() ) : // Show an optional tag description ?>
                         <div class="archive-meta"><?php echo tag_description(); ?></div>
@@ -42,14 +42,10 @@ Template Name: Homepage
                          * this in a child theme then include a file called called content-___.php
                          * (where ___ is the post format) and that will be used instead.
                          */
-                        //get_template_part( 'content', get_post_format() );
+                        get_template_part( 'content');
                 
                 ?>
-                
-                
-		   <h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-                   <?php the_excerpt(); ?>
-                   
+                                   
                 <?php endwhile; ?>
 
         <?php else : ?>
