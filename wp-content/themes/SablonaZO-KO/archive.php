e1ca808034881eda5901fa36e183298076574603
Michal Kobelka <michalkobelka@gmail.com>
2014-05-06 16:37:04 +0200

    Archive

--- wp-content/themes/SablonaZO-KO/archive.php
+++ wp-content/themes/SablonaZO-KO/archive.php
@@ -20,24 +20,38 @@ Template Name: Homepage
 <div class="content-wrapper">
 
 <div class="l-column">
-<!-- box vlevo nahoře -->
-<?php if ( dynamic_sidebar('home-top-left') ) : else : endif; ?>
-
-<!-- /promo-box -->
-
-
-<?php if (is_handheld()) { ?>
-
-    <!-- box vpravo nahoře -->
-	<?php if ( dynamic_sidebar('home-top-right') ) : else : endif; ?>
-	<!-- /media-box -->
-
-<?php } ?>
-
 
 <!-- box vlevo articles -->
 <div id="articles">
-  <?php get_template_part("loop","home"); ?>
+
+    
+        <?php if ( have_posts() ) : ?>
+                <header class="archive-header">
+                        <h1 class="archive-title"><?php printf( __( 'Kategorie: %s' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
+
+                <?php if ( tag_description() ) : // Show an optional tag description ?>
+                        <div class="archive-meta"><?php echo tag_description(); ?></div>
+                <?php endif; ?>
+                </header><!-- .archive-header -->
+
+                <?php
+                /* Start the Loop */
+                while ( have_posts() ) : the_post();
+
+                        /* Include the post format-specific template for the content. If you want to
+                         * this in a child theme then include a file called called content-___.php
+                         * (where ___ is the post format) and that will be used instead.
+                         */
+                        get_template_part( 'content');
+                
+                ?>
+                                   
+                <?php endwhile; ?>
+
+        <?php else : ?>
+                Žádné příspěvky
+        <?php endif; ?>
+
 </div><!-- /articles -->
 
 </div><!-- /l-column -->
