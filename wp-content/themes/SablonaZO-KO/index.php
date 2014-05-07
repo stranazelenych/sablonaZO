24b4d676260b0a854d379a06193ba812e452c545
Michal Kobelka <michalkobelka@gmail.com>
2014-05-07 07:28:19 +0200

    Index

--- wp-content/themes/SablonaZO-KO/index.php
+++ wp-content/themes/SablonaZO-KO/index.php
@@ -26,13 +26,6 @@ Template Name: Homepage
 
     
         <?php if ( have_posts() ) : ?>
-                <header class="archive-header">
-                        <h1 class="archive-title"><?php printf( __( 'Kategorie: %s' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
-
-                <?php if ( tag_description() ) : // Show an optional tag description ?>
-                        <div class="archive-meta"><?php echo tag_description(); ?></div>
-                <?php endif; ?>
-                </header><!-- .archive-header -->
 
                 <?php
                 /* Start the Loop */
