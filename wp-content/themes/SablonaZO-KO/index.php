d5121ed55997c2a5967d55ca0b38d3f57f34edac
Michal Kobelka <michalkobelka@gmail.com>
2014-05-07 07:12:15 +0200

    Index

--- wp-content/themes/SablonaZO-KO/index.php
+++ wp-content/themes/SablonaZO-KO/index.php
@@ -1,3 +1,7 @@
+<?php/*
+Template Name: Homepage
+*/?>
+
 <?php get_header(); ?>
 
 <div class="container main">
@@ -5,141 +9,75 @@
 <div id="menu">
 	<div class="menu-button">Menu</div>
 	<nav>
-      <ul data-breakpoint="800" class="flexnav">
-        <li><a href="">kalendář akcí</a>
-          <ul>
-            <li> <a href="#content">Sub 1 Item 1</a></li>
-            <li><a href="/">Sub 1 Item 2</a></li>
-            <li><a href="/">Sub 1 Item 3</a></li>
-            <li><a href="/">Sub 1 Item 4</a></li>
-          </ul>
-        </li>
-        <li><a href="/">vedení ko zlín</a>
-          <ul>
-            <li><a href="/">Sub 1 Item 1</a></li>
-            <li><a href="/">Sub 1 Item 2</a>
-              <ul>
-                <li><a href="/">Sub 2 Item 1</a></li>
-                <li><a href="http://jasonweaver.name/">Sub 2 Item 2</a></li>
-                <li><a href="http://jasonweaver.name/">Sub 2 Item 3</a></li>
-              </ul>
-            </li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 3</a>
-              <ul>
-                <li><a href="http://jasonweaver.name/">Sub 2 Item 1</a></li>
-                <li><a href="http://jasonweaver.name/">Sub 2 Item 2</a>
-                  <ul>
-                    <li><a href="http://jasonweaver.name/">Sub 3 Item 1</a></li>
-                    <li><a href="http://jasonweaver.name/">Sub 3 Item 2</a></li>
-                    <li><a href="http://jasonweaver.name/">Sub 3 Item 3</a></li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li><a href="http://jasonweaver.name/">základní organizace</a>
-          <ul>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 1</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 2</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 3</a></li>
-          </ul>
-        </li>
-        <li><a href="http://jasonweaver.name/">naši zastupitelé</a>
-          <ul>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 1</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 2</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 3</a></li>
-          </ul>
-        </li>
-        <li><a href="http://jasonweaver.name/">dokumenty</a>
-          <ul>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 1</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 2</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 3</a></li>
-          </ul>
-        </li>
-        <li><a href="http://jasonweaver.name/">zpravodaj</a>
-          <ul>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 1</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 2</a></li>
-            <li><a href="http://jasonweaver.name/">Sub 1 Item 3</a></li>
-          </ul>
-        </li>
-      </ul>
-    </nav>
+		<?php wp_nav_menu( array(
+	        'theme_location' => 'primary',
+	        'menu_class' => 'flexnav', //Adding the class for FlexNav
+	        'items_wrap' => '<ul data-breakpoint="800" id="%1$s" class="%2$s">%3$s</ul>'));
+		?>
+  </nav>
 </div>
 
 <div class="content-wrapper">
 
-<div class="column">
-<!-- box vlevo nahoře -->
-<div id="promo-box" class="box sedy">
-	<h2>Dočasný box pro důležité téma (volby)</h2>
-	<ul class="links">
-		<li><a href="#">Seznamte se: naši kandidáti</a></li>
-		<li><a href="#">Kalendář předvolebních akcí</a></li>
-		<li><a href="#">Zahájení kampaně: fotogalerie</a></li>
-	</ul>
-</div><!-- /promo-box -->
-
-
-<?php if (is_handheld()) { ?>
-
-    <!-- box vpravo nahoře -->
-	<div id="media-box" class="box">
-		<img src="http://placehold.it/920x520" alt=" " />
-	</div><!-- /media-box -->
-
-<?php } ?>
-
+<div class="l-column">
 
 <!-- box vlevo articles -->
 <div id="articles">
-	<article class="highlight">
-		<a href="#"><img src="http://placehold.it/920x520" alt=" " /></a>
-		<h2><a href="#">Brno: Zelení nesouhlasí s plánem na zdražení MHD</a></h2>
-		<p class="perex"><span class="datum-publikovani">21. 7. 2014</span>Strana zelených nesouhlasí s nápadem nového ministra dopravy Pavla Dobeše zavést známky za použití silnic I. třídy, aby získal další prostředky do Státního fondu dopravní infrastruktury. "Okurková sezóna snese mnohé, ale zveřejňovat kdejaký nepromyšlený návrh se nevyplácí," komentuje návrh předseda Ondřej Liška.</p>
-	</article>
-	<article>
-		<img src="http://placehold.it/200x200">
-		<h2><a href="#">Spalování igelitek je výhodné pouze pro průmyslovou lobby</a></h2>
-		<span class="datum-publikovani">2. 12. 2013</span>
-	</article>
-	<article>
-		<img src="http://placehold.it/200x200">
-		<h2><a href="#">Výběrové řízení na dodávku tisků pro volební kampaň do Evropského parlamentu 2014</a></h2>
-		<span class="datum-publikovani"> 1. 1. 2012</span>
-	</article>
+
+    
+        <?php if ( have_posts() ) : ?>
+                <header class="archive-header">
+                        <h1 class="archive-title"><?php printf( __( 'Kategorie: %s' ), '<span>' . single_tag_title( '', false ) . '</span>' ); ?></h1>
+
+                <?php if ( tag_description() ) : // Show an optional tag description ?>
+                        <div class="archive-meta"><?php echo tag_description(); ?></div>
+                <?php endif; ?>
+                </header><!-- .archive-header -->
+
+                <?php
+                /* Start the Loop */
+                while ( have_posts() ) : the_post();
+
+                        /* Include the post format-specific template for the content. If you want to
+                         * this in a child theme then include a file called called content-___.php
+                         * (where ___ is the post format) and that will be used instead.
+                         */
+                        get_template_part( 'content');
+                
+                ?>
+                                   
+                <?php endwhile; ?>
+                
+                <?php paginate(); ?>
+
+        <?php else : ?>
+                Žádné příspěvky
+        <?php endif; ?>
+
 </div><!-- /articles -->
 
-</div><!-- /column -->
+</div><!-- /l-column -->
 
-<div class="column">
+<div class="l-column">
+
 <?php if (!is_handheld()) { ?>
 
     <!-- box vpravo nahoře -->
-	<div id="media-box" class="box">
-		<img src="http://placehold.it/900x700" alt=" " />
-	</div><!-- /media-box -->
+    <?php if ( dynamic_sidebar('home-top-right') ) : else : endif; ?>
+    
+<!-- /media-box -->
 
 <?php } ?>
 
 <!-- sloupce vpravo feeds -->
 <div id="feeds">
 	<div class="column">
-		<div id="peticni-list" class="feed box ">
+		<?php if ( dynamic_sidebar('home-column-first') ) : else : endif; ?>
+	</div>
+		<!-- <div id="peticni-list" class="feed box ">
 			<div class="header">Petiční list</div>
 			<div class="content">
 				<p>Petici za vyřešení hlučnosti silnice I/57 ve Vsetíně Rokytnici můžete podepsat <a href="#" class="read-more">ZDE »</a></p>
-			</div>
-		</div>
-		<div id="novinky-emailem" class="feed box">
-			<div class="header">Novinky emailem</div>
-			<div class="content">
-				<form>
-					<input type="text" placeholder="Váš email" />
-				</form>
 			</div>
 		</div>
 		<div id="socialni-site" class="feed box">
@@ -147,10 +85,10 @@
 			<div class="content">
 				<p>FB box nebo TW stream</p>
 			</div>
-		</div>
-	</div>
+		</div> -->
 	<div class="column">
-		<div id="aktuality" class="feed box">
+		<?php if ( dynamic_sidebar('home-column-second') ) : else : endif; ?>
+		<!-- <div id="aktuality" class="feed box">
 			<div class="header">Aktuality zeleni.cz</div>
 			<div class="content">
 				<ul class="aktuality">
@@ -159,41 +97,18 @@
 					<li><a href="#">Stream.cz: Zachráněné nádraží na Žižkově</a></li>
 				</ul>
 			</div>
-		</div>
+		</div> -->
 	</div>
 
 </div><!-- /feeds -->
 
-</div><!-- /column -->
+</div><!-- /l-column -->
 
 </div><!-- /content-wrapper -->
 
 </div><!-- /container -->
 
-<div class="container">
-	<div class="bottom-nav">
-		<a href="#">kalendář akcí</a>
-		<a href="#">vedení ko zlín</a>
-		<a href="#">základní organizace</a>
-		<a href="#">naši zastupitelé</a>
-		<a href="#">dokumenty</a>
-		<a href="#">zpravodaj</a>
-	</div>
-</div>
-
-<div class="visual">
-    <div class="action-boxes">
-        <div class="box box-info">
-            <h3>Získejte informace</h3>
-        </div>
-        <div class="box box-fundraising">
-            <h3>Podpořte nás</h3>
-        </div>
-    </div>
-</div>
-<div class="visual-footer">
-    <p>Ing.Vilém Jurek, předseda KO  |  tel. 605 526 958  |  email: <a href="#">vilem.j@gmail.com</a></p>
-</div>
+<?php include_once('visual.php'); ?>
 
 
 <?php get_footer(); ?>
