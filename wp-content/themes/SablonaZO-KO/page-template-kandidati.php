5cef7eb03b8e97c099ea0deef7d86f5ddb2259bf
Michal Berg <michal.berg@esports.cz>
2014-08-28 23:28:46 +0200

    uprava šablony kandidáti

--- wp-content/themes/SablonaZO-KO/page-template-kandidati.php
+++ wp-content/themes/SablonaZO-KO/page-template-kandidati.php
@@ -1,15 +1,29 @@
+
 <?php
 /*
-Template Name: Kandidáti
+Template Name: homepage
 */
 ?>
+
 <?php get_header(); ?>
-      <div>
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-          <?php the_post_thumbnail(); ?></div>
-          <?php the_content(); ?></p>
-        <?php endwhile; ?>
-        <?php endif; ?>
+
+<div class="container main">
+
+<div id="menu">
+	<div class="menu-button">Menu</div>
+	<nav>
+		<?php wp_nav_menu( array(
+	        'theme_location' => 'primary',
+	        'menu_class' => 'flexnav', //Adding the class for FlexNav
+	        'items_wrap' => '<ul data-breakpoint="800" id="%1$s" class="%2$s">%3$s</ul>'));
+		?>
+  </nav>
+</div>
+    
+<div class="content-wrapper">
+
+
+
 <?php
  //logika zobrazování custom fieldu true/false
 if( get_field('boxx') )
@@ -19,6 +33,24 @@ if( get_field('boxx') )
     echo '<div style="width: 200px; height: 200px; background: #000;">';
 }
 ?>
-      </div>
+
+
+
+
+
+<div class="l-aside">
+    
+    <?php if ( dynamic_sidebar('post-page-right') ) : else : endif; ?>
+    
+</div><!-- /l-aside -->
+
+<?php endwhile; ?>
+<?php endif; ?>
+
+</div><!-- /content-wrapper -->
+
+</div><!-- /container -->
+
+<?php include_once('visual.php'); ?>
 
 <?php get_footer(); ?>
