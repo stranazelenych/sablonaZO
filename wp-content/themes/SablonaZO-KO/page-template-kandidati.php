21922cc78cde5eb1a0c1db1de034d1c8d570c83f
Michal Berg <michal.berg@esports.cz>
2014-08-29 09:38:19 +0200

    Update page-template-kandidati.php

--- wp-content/themes/SablonaZO-KO/page-template-kandidati.php
+++ wp-content/themes/SablonaZO-KO/page-template-kandidati.php
@@ -140,7 +140,7 @@ if (get_sub_field('is_member')==1) {
 $photo_url = get_sub_field('photo');
 
         if (!empty($photo_url)) {
-        echo "<img src=" . $photo_url . " align=left width=400 >" ;
+        echo "<img src=" . $photo_url . " align=left width=400 style=margin-bottom:20px><br>" ;
         } 
         
         ?>      
