1ed4893e29ea9f5d00d4be1396615efd4be8659b
Michal Berg <michal.berg@esports.cz>
2016-02-18 16:27:08 +0100

    úprava stránkování - homepage

--- wp-content/themes/SablonaZO-KO/index.php
+++ wp-content/themes/SablonaZO-KO/index.php
@@ -41,7 +41,9 @@ Template Name: Homepage
                                    
                 <?php endwhile; ?>
                 
-                <?php paginate_links(); ?>
+              	<div class="nav-previous alignleft"><?php next_posts_link( 'Older posts' ); ?></div>
+		<div class="nav-next alignright"><?php previous_posts_link( 'Newer posts' ); ?></div>
+
 
         <?php else : ?>
                 Žádné příspěvky
